3be8f1a9c1dee48abb8c786f87c224760a5b14e2
Quy Nguyen <steve.parker117@gmail.com>
2022-03-26 18:03:25 +0700

    fix: delete permantly ```TodoContext.cs``` file.
    BREAKING CHANGE: add Account entity and BaseEntity

---
+++ FFStudioServices/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FFStudioServices.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FFStudioServices.Models
+{
+    public class Account: BaseEntity
+    {
+        [Key]
+        public string UserId { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Column(TypeName = "json")]
+        public string info { get; set; }
+    }
+}
---
+++ FFStudioServices/Models/BaseModels/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FFStudioServices.Models.BaseModels
+{
+    public class BaseEntity
+    {
+        [Required]
+        public DateTime Created_Date { get; set; }
+        [Required]
+        public DateTime Updated_Date { get; set; }
+    }
+}
