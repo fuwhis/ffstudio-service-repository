6bd87c996609c8081903ad23649e7ce4125c5f1e
Quy Nguyen <steve.parker117@gmail.com>
2022-04-02 21:55:44 +0700

    `build!`: add a base class & interface of generic repository following by Repository Pattern

---
+++ FFStudioServices/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using FFStudioServices.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FFStudioServices.Repositories
+{
+    public class GenericRepository<T>: IGenericRepository<T> where T : class
+    {
+        protected PostgreContext _context;
+        internal DbSet<T> dbSet;
+        protected readonly ILogger _logger;
+
+        public GenericRepository(PostgreContext context, ILogger logger)
+        {
+            this._context = context;
+            this.dbSet = context.Set<T>();
+            this._logger = logger;
+
+        }
+
+        public virtual Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<T> GetById(object id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public Task<bool> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Upsert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FFStudioServices/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FFStudioServices.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(object id);
+        Task<bool> Add(T entity);
+        Task<bool> Delete(object id);
+        Task<bool> Upsert(T entity);
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+    }
+}
