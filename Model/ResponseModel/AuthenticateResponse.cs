94ce8c49e08410e6a1ff63fb8595cbfedf75fe39
Quy Nguyen <steve.parker117@gmail.com>
2022-05-08 00:47:28 +0700

    `build`: add Jwt authentication request model and response model for implementation

---
+++ Model/ResponseModel/AuthenticateResponse.cs
@@ -0,0 +1,29 @@
+﻿using EntityModel.Models;
+using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ResponseModel
+{
+    public class AuthenticateResponse
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+
+        //[Column(TypeName = "json")]
+        public string? Info { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponse(Account account, string token)
+        {
+            Id = account.UserId;
+            Name = account.Name; 
+            Info = account.Info;
+            Token = token;
+        }
+    }
+}
